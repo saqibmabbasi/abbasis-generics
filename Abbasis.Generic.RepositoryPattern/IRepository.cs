f2ca745b07758c55984e57d074e3e8880d6f14e2
Saqib Mustafa Abbasi <saqibmabbasi@gmail.com>
2024-10-26 12:29:34 +0500

    Added Repository Pattern code

---
+++ Abbasis.Generic.RepositoryPattern/IRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abbasis.Generic.RepositoryPattern
+{
+	internal interface IRepository<TEntity> where TEntity : class
+	{
+		TEntity GetById(long id);
+
+		IEnumerable<TEntity> GetAll();
+
+		long GetCount();
+
+		IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate);
+
+		void Add(TEntity entity);
+
+		void AddRange(IEnumerable<TEntity> entities);
+
+		void Remove(TEntity entity);
+
+		void RemoveRange(IEnumerable<TEntity> entities);
+	}
+}
---
+++ Abbasis.Generic.RepositoryPattern/Repository.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Abbasis.Generic.RepositoryPattern
+{
+	public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+	{
+		protected readonly DbContext context;
+
+
+		public Repository(DbContext _context)
+		{
+			context = _context;
+		}
+
+
+		public void Add(TEntity entity)
+		{
+			context.Entry(entity).State = EntityState.Added;
+			context.Set<TEntity>().Add(entity);
+		}
+
+		public void AddRange(IEnumerable<TEntity> entities)
+		{
+			context.Set<TEntity>().AddRange(entities);
+		}
+
+		public IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate)
+		{
+			return context.Set<TEntity>().Where(predicate);
+		}
+
+		public TEntity GetById(long id)
+		{
+			return context.Set<TEntity>().Find(id);
+		}
+
+		public IEnumerable<TEntity> GetAll()
+		{
+			return context.Set<TEntity>().ToList();
+		}
+
+
+		public long GetCount()
+		{
+			return context.Set<TEntity>().Count();
+		}
+
+		public void Remove(TEntity entity)
+		{
+			context.Set<TEntity>().Remove(entity);
+		}
+
+		public void RemoveRange(IEnumerable<TEntity> entities)
+		{
+			context.Set<TEntity>().RemoveRange(entities);
+		}
+	}
+}
