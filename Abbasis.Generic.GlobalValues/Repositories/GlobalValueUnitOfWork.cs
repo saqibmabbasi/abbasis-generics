4327a72395deb97bf3fa6b4bf778e270c891c883
Saqib Mustafa Abbasi <saqibmabbasi@gmail.com>
2024-10-27 11:14:54 +0500

    Unit of Work added to Global Values

---
+++ Abbasis.Generic.GlobalValues/Repositories/GlobalValueUnitOfWork.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Abbasis.Generic.GlobalValues.Repositories
+{
+	public interface IGlobalValueUnitOfWork : IDisposable
+	{
+		IFndGlobalValuesRepository globalValuesRepository { get; }
+		int Complete();
+	}
+
+
+	public class GlobalValueUnitOfWork: IGlobalValueUnitOfWork
+	{
+		private readonly DbContext context;
+
+		public IFndGlobalValuesRepository globalValuesRepository { get; }
+
+        public GlobalValueUnitOfWork(DbContext _context)
+        {
+			this.context = _context;
+			globalValuesRepository = new FndGlobalValuesRepository(_context);
+		}
+
+
+        public int Complete()
+		{
+			return context.SaveChanges();
+		}
+
+		public void Dispose()
+		{
+			context.Dispose();
+		}
+	}
+}
